d94a5ce254aebd3a42126ea51c19c8056b27da61
RbMo7 <rbmohit1283@gmail.com>
2024-10-18 21:04:54 +0545

    Json render

---
+++ src/sections/LandingSection/components/TestimonialsSection/TestimonialConts.ts
@@ -0,0 +1,24 @@
+import test from "node:test";
+
+export const TESTIMONIALS = [
+    {
+        img: '',
+        name: 'Bibhusan Saakha',
+        testimonial: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis.'
+    },
+    {
+        img: '',
+        name: 'Bibhusan Saakha',
+        testimonial: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis.'
+    },
+    {
+        img: '',
+        name: 'Bibhusan Saakha',
+        testimonial: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis.'
+    },
+    {
+        img: '',
+        name: 'Bibhusan Saakha',
+        testimonial: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis.'
+    },
+];
--- src/sections/LandingSection/components/TestimonialsSection/index.tsx
+++ src/sections/LandingSection/components/TestimonialsSection/index.tsx
@@ -1,14 +1,16 @@
 import TestimonialModal from "@/sections/LandingSection/components/TestimonialsSection/TestimonialModal";
+import { TESTIMONIALS } from "./TestimonialConts";
 
 export default function TestimonialsSection() {
     return (
         <div className="flex flex-col gap-[24px] ">
             <h1 className="text-[48px] md:text-[72px] leading-9 font-normal text-white">Let's hear from the <span className="text-gold">past participants</span>!</h1>
             <div className="flex gap-[16px] flex-wrap justify-center ">
-            <TestimonialModal img="" testimonial="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis." name="Bibhusan Saakha"/>
-            <TestimonialModal img="" testimonial="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis." name="Bibhusan Saakha"/>
-            <TestimonialModal img="" testimonial="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis." name="Bibhusan Saakha"/>
-            <TestimonialModal img="" testimonial="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore incidunt cupiditate laboriosam optio porro quaerat, dolores, eius aliquam perferendis corporis, suscipit nobis. Sequi modi corrupti provident minima nesciunt! Totam, officiis." name="Bibhusan Saakha"/>
+            {
+                TESTIMONIALS.map((testimonial) => (
+                    <TestimonialModal key={testimonial.name} img={testimonial.img} testimonial={testimonial.testimonial} name={testimonial.name}/>
+                ))
+            }
             </div>
         </div>
     )
