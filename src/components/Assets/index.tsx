a52cebe54abc8aa4f3eff0925f28dd4970dc4ae7
Manjul Tamrakar <manjultamrakar4@gmail.com>
2024-10-18 23:30:17 +0545

    added all required components

---
+++ src/components/Assets/index.tsx
@@ -0,0 +1,19 @@
+import Heart from "../../../public/images/heart.png";
+import TreasureOpen from "../../../public/images/open-treasure.png"
+import City from "../../../public/images/city.png";
+import NepalCity from "../../../public/images/nepalCity.png";
+import CoinSparkle from "../../../public/images/coinSparkle.png";
+import DiamondSword from "../../../public/images/diamondSword.png";
+import ClickArea from "../../../public/images/clickable.png"
+import Book from "../../../public/images/book.png";
+
+export {
+    Heart,
+    TreasureOpen,
+    City,
+    NepalCity,
+    CoinSparkle,
+    DiamondSword,
+    ClickArea,
+    Book,
+}
--- src/sections/LandingSection/components/PastIterationSection/index.tsx
+++ src/sections/LandingSection/components/PastIterationSection/index.tsx
@@ -1,5 +1,11 @@
+import Iteration22 from "./2022Iteration";
+import Iteration23 from "./2023Iteration";
+
 export default function PastIterationSection() {
     return (
-        <div>PastIterationSection</div>
-    )
+        <>
+            <Iteration23 />
+            <Iteration22 />
+        </>
+    );
 }
