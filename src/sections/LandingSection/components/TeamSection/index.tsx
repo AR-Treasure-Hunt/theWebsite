8613ab739b2d56ff7c431b59a314db88f8477cc4
David Ramovich Mandal <davidramovichmandal@gmail.com>
2024-10-19 21:17:35 +0545

    first card of the team section , more to come , image for test only hehe

--- src/sections/LandingSection/components/TeamSection/index.tsx
+++ src/sections/LandingSection/components/TeamSection/index.tsx
@@ -1,5 +1,32 @@
-export default function TeamSection() {
-    return (
-        <div>TeamSection</div>
-    )
-}
+// team.tsx
+import React from 'react';
+import './team.css';
+import 'font-awesome/css/font-awesome.min.css'; // Importing FontAwesome
+
+const Team: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <div className="single-card">
+        <div className="front"></div>
+        <div className="back">
+          <div className="content">
+            <h2>Mark Jones</h2>
+            <h4>Web Developer</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut molestias quaerat consectetur. Voluptas sed,
+              aliquam reiciendis odio, labore quod itaque.
+            </p>
+            <p className="socials">
+              <i className="fa fa-youtube"></i>
+              <i className="fa fa-instagram"></i>
+              <i className="fa fa-linkedin"></i>
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
