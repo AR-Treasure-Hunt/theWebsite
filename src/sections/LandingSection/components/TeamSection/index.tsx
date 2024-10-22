c23c5a5f18e4e921d0f2fcd790d7f30acb93aa2c
David Ramovich Mandal <davidramovichmandal@gmail.com>
2024-10-22 15:38:07 +0545

    sabai janako social links rakhdeko chu . upto number 34

--- src/sections/LandingSection/components/TeamSection/index.tsx
+++ src/sections/LandingSection/components/TeamSection/index.tsx
@@ -6,7 +6,7 @@ import backgroundImage from './background.png'; // Import the background image
 
 // Interface for team member items
 interface SocialLinks {
-  youtube?: string;
+  github?: string;
   instagram?: string;
   linkedin?: string;
 }
@@ -25,61 +25,82 @@ const teamData: TeamMember[] = [
     name: 'Bibhushan Saakha',
     role: 'Event Lead',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
-    socialLinks: {
-      youtube: 'https://youtube.com',
-      instagram: 'https://instagram.com',
-      linkedin: 'https://linkedin.com/'
-    }
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
   },
   {
     name: 'Nilotpal Dhakal',
     role: 'Talent Manager',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+      socialLinks: {
+        instagram: 'https://www.instagram.com/nilotpaldhakal/'
+      }},
   {
     name: 'Ankit Kumar Karn',
     role: 'Social Media Manager',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/Ankit0581206',
+      instagram: 'https://www.instagram.com/ankit_karn.568/profilecard/?igsh=MWJtd2d2YXAxMmF6dQ==',
+      linkedin: 'https://www.linkedin.com/in/ankit-kumar-karn-679198251'
+    }},
   {
     name: 'Royas Shakya',
     role: 'Talent Manager',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    },
   {
     name: 'Sinja Ghimire ',
     role: 'Content Creator',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/sinjaghimire',
+      instagram: 'https://www.instagram.com/sinjaghimire/',
+      linkedin: 'https://www.linkedin.com/in/sinja-ghimire-21b4a9239/'
+    }},
   {
     name: 'Subani Malla',
     role: 'Character and Background Design ',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/SubaniMalla',
+      instagram: 'https://www.instagram.com/subani_malla/',
+      linkedin: 'https://www.linkedin.com/in/subani-malla-b311a7178/'
+    }},
   {
     name: 'Abi Shrestha',
     role: 'Web Development Lead',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/scifisatan',
+      instagram: 'https://www.instagram.com/39abii/',
+      linkedin: 'https://www.linkedin.com/in/39abii/'
+    }},
   {
     name: 'Himesh Dulal ',
     role: 'Backend Developer ',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/Githeimer',
+      instagram: 'https://www.instagram.com/hiimesh.exe/',
+      linkedin: 'https://www.linkedin.com/in/himesh-dulal-2a6176277/'
+    }},
   {
     name: 'Manjul Tamrakar',
     role: 'Frontend Developer',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/Tamrakar182',
+      instagram: 'https://www.instagram.com/manjul.tamrakar/',
+      linkedin: 'https://www.linkedin.com/in/manjul-tamrakar/'
+    }},
   {
     name: 'Neo Shakya',
     role: 'Character and Background Design',
@@ -90,8 +111,12 @@ const teamData: TeamMember[] = [
     name: 'Sushila Shrestha ',
     role: 'Frontend Developer',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/sushilashrestha',
+      instagram: 'https://www.instagram.com/__susshhh__/',
+      linkedin: 'https://www.linkedin.com/in/sushilashrestha/'
+    }},
   {
     name: 'Anuja Lamsal',
     role: 'Backend Developer',
@@ -132,14 +157,22 @@ const teamData: TeamMember[] = [
     name: 'Angelina Ghimire',
     role: 'Creative Lead',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/angelinaghimire',
+      instagram: 'https://www.instagram.com/angelinaghimire/',
+      linkedin: 'https://www.linkedin.com/in/angelina-ghimire/'
+    }},
   {
     name: 'Sujay Pal',
     role: 'Marketing Lead',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'http://github.com/sujayp178',
+      instagram: 'https://www.instagram.com/sujay_pl19/profilecard/?igsh=aXdpaXh6Ym1icmNu',
+      linkedin: 'https://www.linkedin.com/in/pal-sujay-66171228a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
+    }},
   {
     name: 'Samikshya Upadhyay',
     role: 'Talent Management',
@@ -156,38 +189,55 @@ const teamData: TeamMember[] = [
     name: 'Regal Adhikari',
     role: 'Game development Lead',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/regaladhikari',
+      linkedin: 'https://www.linkedin.com/in/regal-adhikari-03b958254/'
+    }},
   {
     name: 'Aakriti Banjara',
     role: 'Game Development',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      github: 'https://github.com/Aakriti555',
+      linkedin: 'https://www.linkedin.com/in/aakriti-banjara-2b5b66240/'
+    }},
   {
     name: 'Swarnim Bajracharya',
     role: 'Creative Team Member',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+  socialLinks: {
+    github: 'https://github.com/SwarnimBajra',
+    instagram: 'https://www.instagram.com/sm_bajracharya?igsh=ejczdnFpaWFmNXV1',
+    linkedin: 'https://www.linkedin.com/in/swarnim-bajracharya-7a5727288/'
+  }},
   {
     name: 'Sally Shrestha',
     role: '?????',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+  socialLinks: {
+    instagram: 'https://www.instagram.com/miiruex__/?hl=en',
+    linkedin: 'https://www.linkedin.com/in/sally-shrestha-54a319300/'
+  }},
   {
     name: 'Rohit Karki',
     role: '?????',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+ },
   {
     name: 'Albina Shrestha',
     role: 'Character and Background Design',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+      socialLinks: {
+        github: 'https://github.com/alberrrry',
+        instagram: 'https://www.instagram.com/alberrrry/',
+        linkedin: 'https://www.linkedin.com/in/albina-shrestha-75955b2a1/'
+      }},
   {
     name: 'Shreejan Prasad Karmacharya',
     role: 'Content Creator',
@@ -198,8 +248,10 @@ const teamData: TeamMember[] = [
     name: 'Kshitiz Khanal',
     role: '?????',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+    socialLinks: {
+      instagram: 'https://www.instagram.com/chhitiz9296/'
+    }},
   {
     name: 'Nirjal Bhurtel',
     role: 'App developer',
@@ -210,14 +262,31 @@ const teamData: TeamMember[] = [
     name: 'Arpan Aadhikari',
     role: '?????',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  },
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+  socialLinks: {
+    github: 'https://github.com/Arpan-Adhikari',
+    instagram: 'https://www.instagram.com/adhikari_arpan11/',
+    linkedin: 'https://www.linkedin.com/in/arpan-adhikari-2589962b2/'
+  }},
   {
     name: 'Nischal Baral',
     role: 'App Developer',
     quote:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-  }
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+  socialLinks: {
+    github: 'https://github.com/Anonymous719',
+    instagram: 'https://www.instagram.com/nischal__baral/',
+    linkedin: 'https://www.linkedin.com/in/nischal-baral-6b8404229/'
+  }},
+  {
+    name: 'bhishma Bhandari',
+    role: 'Sponsorship Member',
+    quote:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
+  socialLinks: {
+    github: 'https://github.com/DontHash',
+    instagram: 'https://www.instagram.com/rhy_tham/'
+  }}
 ];
 
 const Team: React.FC = () => {
@@ -248,9 +317,9 @@ const Team: React.FC = () => {
                     <p>{member.quote}</p>
                   </div>
                   <p className="socials">
-                    {member.socialLinks?.youtube && (
+                    {member.socialLinks?.github && (
                       <a
-                        href={member.socialLinks.youtube}
+                        href={member.socialLinks.github}
                         target="_blank"
                         rel="noopener noreferrer">
                         <i className="fa fa-youtube"></i>
